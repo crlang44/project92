ef81e10fba8a448410513327802c6ea5d95c56d9
crlang44 <crlang44@gmail.com>
2019-08-18 12:35:01 -0700

    Added field animation demo

--- src/field.ts
+++ src/field.ts
@@ -17,10 +17,19 @@ export class Field implements Drawable{
         let css =
         `<style>
         .field{
-            width: 90vw;
-            background-color: green;
-            height: 60vh;
+            width: 100vw;
+            height: 100vh;
+            background-image: url(https://i0.wp.com/avantisocceracademy.com/wp-content/uploads/2018/05/soccer-field-wallpaper-high-resolution-38744812.jpg?ssl=1);
+            background-size: contain;
+            margin: auto;
+            transform: rotateX(10deg) translateZ(-200px);
+            animation: x-axis-animation 10s ease-in-out 0s infinite alternate;
         }
+
+        @keyframes x-axis-animation {
+            from {transform: translateX(200px) rotateX(10deg) translateZ(-200px)}
+            to {transform: translateX(-200px) rotateX(10deg) translateZ(-200px)}
+          }
         </style>`
 
         let html = 
